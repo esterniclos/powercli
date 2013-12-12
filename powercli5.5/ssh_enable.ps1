ebc029aca7d869fa2f490b157b8efbfd4ff22285
esterniclos@gmail.com <esterniclos@gmail.com>
2013-12-12 08:01:37 +0100

    ssh console enable and disable scripts

---
+++ powercli5.5/ssh_enable.ps1
@@ -0,0 +1,13 @@
+#
+# based on : http://www.techshifter.com/2012/01/11/vsphere-powercli-mass-enabling-disabling-of-ssh-on-vsphere-hosts/
+#
+# Ester Niclos Ferreras
+# Last updated: 12/12/13
+
+
+Get-Cluster | Foreach {
+$cl=$_
+Get-Cluster -name $cl | Get-VMHost | Foreach { 
+         Start-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" } ) 
+		 }
+}
