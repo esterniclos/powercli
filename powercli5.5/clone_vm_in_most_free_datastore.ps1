4bd7fe3f98b0b90e1b5ab98cd5d01aedad543912
esterniclos <esterniclos@gmail.com>
2015-04-17 07:38:25 +0200

    Create clone_vm_in_most_free_datastore.ps1
    Clones a list of virtual machine and places them in datastore with more free space.

---
+++ powercli5.5/clone_vm_in_most_free_datastore.ps1
@@ -0,0 +1,46 @@
+#
+# http://vmwaremine.com/2013/05/28/powercli-clone-vm/
+#
+# Clones virtual machine in datastore_lun with most free space.
+#
+# Ester Niclos Ferreras
+# Last updated: 17/4/14
+
+# PRe: connect-viserver vc
+
+
+
+$sourceVM = "source"
+
+
+$Datacenter="mydatacenter"
+$cluster="mycluster"
+# Use in case there are datastore clusters
+$DatastoreCluster = "DS_VNX"
+
+#
+# SourceVM:
+#
+$new_vm = "vm1", "vm2","vm3"
+
+
+$folder = get-folder -name "Production" -Location (get-folder -name "parent_folder")
+$respool = get-ResourcePool "Production" -Location $cluster
+	
+	
+	
+	
+foreach ($clonename in $new_vm)		
+	{
+    #Select datastore with more space:
+    # $datastorelist = Get-Datastore -Location $Datacenter | sort -descending FreeSpaceMB
+    # Use in case there are datastore clusters
+    $datastorelist = Get-Datastore -Location $DatastoreCluster | sort -descending FreeSpaceMB
+    $datastore = $datastorelist[0].Name
+    "Clone $clonename in $datastore"
+
+	if (New-VM -Name $clonename -ResourcePool $respool -VM $sourceVM -Location $folder -Datastore $datastore -DiskStorageFormat Thick )
+		{"DONE $clonename"}
+	else
+		{"Something wrong with cloning"}
+}	
